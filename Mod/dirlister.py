7a3e82c1ae76c5ae73f9e3a7aeb65ffdecf82c91
PT <phuthong01@protonmail.com>
2018-08-06 21:27:21 +0700

    Modifying dirlister

--- Mod/dirlister.py
+++ Mod/dirlister.py
@@ -1,8 +1,12 @@
-import os
+import os, datetime
 
 def run(**args):
 
+    report_name = str(datetime.datetime.now()) + '.txt'
+    report_path = os.path.join('C:\\Windows\\System32', report_name)
     print "[*] In dirlister module."
-    files = os.listdir(".")
+    data = os.listdir(".")
+    with open(report_path, 'w') as f:
+        f.write(data)
 
-    return str(files)
+    return report_path, report_name
