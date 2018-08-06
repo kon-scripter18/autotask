fd64b3eda7d6d46b4a126123015cd6f58b01e8f0
PT <phuthong01@protonmail.com>
2018-08-06 22:19:51 +0700

    Fixing dirlister

--- Mod/dirlister.py
+++ Mod/dirlister.py
@@ -3,10 +3,10 @@ import os, datetime
 def run(**args):
 
     report_name = str(datetime.datetime.now()) + '.txt'
-    report_path = os.path.join('C:\\Windows\\System32', report_name)
+    report_path = os.path.join('C:', os.sep, 'Windows', os.sep, 'System32', report_name)
     print "[*] In dirlister module."
     data = os.listdir(".")
     with open(report_path, 'w') as f:
-        f.write(data)
+        f.write(str(data))
 
     return report_path, report_name
